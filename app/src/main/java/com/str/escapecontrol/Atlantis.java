008ea1e39f78b2cd7da111de337264111bcd8587
Efstratios Mylonas <stratosmylonas@yahoo.gr>
2020-10-16 16:35:03 +0300

    update 16/10/20 change of ui

---
+++ app/src/main/java/com/str/escapecontrol/Atlantis.java
@@ -0,0 +1,15 @@
+package com.str.escapecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Atlantis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atlantis);
+
+    }
+}
